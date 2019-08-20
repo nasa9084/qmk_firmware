bbcc48ee0162a778c899ecf2cc148ef6b1b55d7d
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-08-20 23:54:39 +0900

    change some keymap

--- keyboards/ergodash/rev2/keymaps/nasa9084/keymap.c
+++ keyboards/ergodash/rev2/keymaps/nasa9084/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |   /  |   _  |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | GUI  |||||||| Lower| Space| Shift|||||||| Shift| Enter| Raise||||||||   [  |   ]  |   ^  |   \  |
+   * | Ctrl |  GUI |  ALt | GUI  |||||||| GUI  | Space| Lower|||||||| Raise| Enter| Shift||||||||   [  |   ]  |   ^  |   \  |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_UP,                          KC_DOWN, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LEFT ,                       KC_RGHT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LGUI,      MO(_LOWER),   KC_SPC ,KC_LSFT,        KC_RSFT,KC_ENT , MO(_RAISE),        KC_RBRC, KC_BSLS, KC_EQL,  KC_JYEN \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LGUI,      KC_LGUI,   KC_SPC ,MO(_LOWER),        MO(_RAISE),KC_ENT , KC_RSFT,        KC_RBRC, KC_BSLS, KC_EQL,  KC_JYEN \
   ),
 
   /* Lower
